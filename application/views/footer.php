50049f7fd8ccabcee71dd73d694044b54052fcab
unknown <kurniadrajat0@gmail.com>
2021-03-18 12:15:49 +0700

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,17 @@
+    <!-- Footer -->
+    <footer class="bg-dark">
+        <div class="container-footer">
+            <div class="small text-center text-muted">Copyright 2021 - A2T</div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+</html>
